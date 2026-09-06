913885c5f6ef2d6d0ebfb0612541b24659b30a70
I'TS RASHMI <itsrashmi4@gmail.com>
2026-09-06 12:43:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,12 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "")
     MONGO_URI = os.environ.get("MONGO_URI", "")
     ADMIN_ID = int(os.environ.get("ADMIN_ID", 0))
-    PORT = int(os.environ.get("PORT", 8000)) # Koyeb defaults to 8000
+    PORT = int(os.environ.get("PORT", 8000)) # Koyeb / Render defaults
     
     # --- ADDED FOR THE NEW WORKFLOW ---
     DUMP_CHANNEL_ID = int(os.environ.get("DUMP_CHANNEL_ID", 0)) # e.g., -100123456789
     TMDB_API_KEY = os.environ.get("TMDB_API_KEY", "") # Get free from themoviedb.org
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "") # e.g., YourBotName without the @
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "") # Your Admin bot username (no @)
+    
+    # --- NEW FILE STORE BOT ---
+    FILE_STORE_BOT_USERNAME = os.environ.get("FILE_STORE_BOT_USERNAME", "") # Your File Bot username (no @)
